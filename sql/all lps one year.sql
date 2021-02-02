f9b43641b3771a173ca12387726f2449eb84b3b0
Kevin Hausmann <kevin.hausmann@uba.de>
2021-02-02 08:16:41 +0100

    Create queries that actually do something useful

---
+++ sql/all lps one year.sql
@@ -0,0 +1,18 @@
+﻿SELECT
+	name,
+	prtr_key AS 'GNFR (TODO)',
+	administrative_number AS 'E-PRTR ID',
+	'' AS 'Height class (TODO)',
+	wgs84_x, wgs84_y,
+	SUM(annual_load) FILTER (WHERE substance_name like 'Arsen%') AS "Arsen (kg)",
+	SUM(annual_load / 1000000) FILTER (WHERE substance_name like 'Carbon dioxide%') AS "CO2 (kt)",
+	SUM(annual_load / 1000) FILTER (WHERE substance_name like 'Sulphur%') AS "SO2 (t)"
+FROM (
+	SELECT facilities.id, name, prtr_key, wgs84_x, wgs84_y, administrative_number, releases.year, substance_name, annual_load, releases.unit
+	FROM facilities
+	INNER JOIN activities ON facilities.id = activities.facility_id
+	INNER JOIN releases ON facilities.id = releases.facility_id
+	WHERE releases.year = 2018)
+GROUP BY id
+/* ORDER BY name */
+ORDER BY "SO2 (t)" DESC
---
+++ sql/one lps many years.sql
@@ -0,0 +1,13 @@
+﻿SELECT name, year, prtr_key, wgs84_x, wgs84_y,
+	SUM(annual_load) FILTER (WHERE substance_name like 'Arsen%') Arsen,
+	SUM(annual_load) FILTER (WHERE substance_name like 'Carbon dioxide%') CO2,
+	SUM(annual_load) FILTER (WHERE substance_name like 'Sulphur%') SO2
+FROM (
+	SELECT facilities.id, facilities.name, facilities.wgs84_x, facilities.wgs84_y, activities.prtr_key, releases.year, releases.substance_name, releases.annual_load, releases.unit
+	FROM facilities 
+	INNER JOIN releases ON facilities.id = releases.facility_id
+	INNER JOIN activities ON facilities.id = activities.facility_id
+	WHERE facilities.name like '%Lippendorf%'
+	)
+GROUP BY year
+ORDER BY year
