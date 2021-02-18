eb5cd87c8c7af32b3dcd41e5055357340a253cd2
Kevin Hausmann <kevin.hausmann@uba.de>
2021-02-18 14:31:21 +0100

    Simplify query and fix NMVOC/SO2 mistake. Also we are now filtering for main_activity == True to avoid double counting.

--- sql/all lps one year.sql
+++ sql/all lps one year.sql
@@ -7,8 +7,8 @@
 	'' AS 'Height class (TODO)',
 	wgs84_x AS 'Longitude', wgs84_y AS 'Latitude',
 	SUM(annual_load / 1000000) FILTER (WHERE substance_name = 'Nitrogen oxides (NOx/NO2)') AS 'NOx (as NO2) [kt]',
-	SUM(annual_load / 1000000) FILTER (WHERE substance_name = 'Sulphur oxides (SOx/SO2)') AS 'NMVOC [kt]',
-	SUM(annual_load / 1000000) FILTER (WHERE substance_name = 'Non-methane volatile organic compounds (NMVOC)') AS 'SOx (as SO2) [kt]',
+	SUM(annual_load / 1000000) FILTER (WHERE substance_name = 'Sulphur oxides (SOx/SO2)') AS 'SOx (as SO2) [kt]',
+	SUM(annual_load / 1000000) FILTER (WHERE substance_name = 'Non-methane volatile organic compounds (NMVOC)') AS 'NMVOC [kt]',
 	SUM(annual_load / 1000000) FILTER (WHERE substance_name = 'Ammonia (NH3)') AS 'NH3 [kt]',
 	SUM(annual_load / 1000000) FILTER (WHERE substance_name = 'Particulate matter (PM2.5)') AS 'PM2.5 [kt]',
 	SUM(annual_load / 1000000) FILTER (WHERE substance_name = 'Particulate matter (PM10)') AS 'PM10 [kt]',
@@ -20,11 +20,9 @@
 	SUM(annual_load / 1000) FILTER (WHERE substance_name = 'Polycyclic aromatic hydrocarbons (PAHs)') AS 'PAHs [t]',
 	SUM(annual_load) FILTER (WHERE substance_name = 'Hexachlorobenzene (HCB)') AS 'HCB [kg]',
 	SUM(annual_load) FILTER (WHERE substance_name = 'Polychlorinated biphenyls (PCBs)') AS 'PCBs [kg]'
-FROM (
-	SELECT facilities.id, name, nace_code, nace_text, prtr_key, wgs84_x, wgs84_y, administrative_number, substance_name, annual_load
-	FROM facilities
+FROM facilities
 	INNER JOIN activities ON facilities.id = activities.facility_id
 	INNER JOIN releases ON facilities.id = releases.facility_id
-	WHERE facilities.year = 2018 and releases.compartment = 'Air')
-GROUP BY id
+WHERE facilities.year = 2018 and releases.compartment = 'Air' and activities.main_activity = True
+GROUP BY facilities.id
 ORDER BY name
